d80f781b7cd1097023a623e5f2e1a67354efdc0d
adisezhm <adisezhm@gmail.com>
2018-12-26 18:03:41 +0530

    Get /listOneProject from database

--- angular.js/controller/agile.js
+++ angular.js/controller/agile.js
@@ -14,41 +14,54 @@ agileApp.config(['$routeProvider', function($routeProvider) {
 	})
 }]);
 
-agileApp.controller('oneProjectController', function($scope) {
-	$scope.project = {
-		name: "k4",
-		sprints:[
-			{sid:10, sStatus:'active', sName:'A-a123456789012345'},
-			{sid:20, sStatus:'active', sName:'B'},
-			{sid:30, sStatus:'active', sName:'C'},
-			{sid:40, sStatus:'active', sName:'d'},
-		]
-	};
-});
-
 agileApp.controller('allProjectsController', ['$scope','agileService', function ($scope, agileService)
 {
-    $scope.getAllProjects = function () {
-        agileService.getAllProjects()
-          .then(function success(response){
-              $scope.projects = response.data;
-              $scope.p2 = "This is P2.. intentional !!";
-              $scope.errorMessage = '';
-          },
-          function error (response ){
-              $scope.errorMessage = 'Error getting projects!';
-          });
-    }
+	$scope.getAllProjects = function () {
+		agileService.getAllProjects()
+		.then(function success(response){
+			$scope.projects = response.data;
+			$scope.p2 = "This is P2.. intentional !!";
+			$scope.errorMessage = '';
+		},
+		function error (response ){
+			$scope.errorMessage = 'Error getting projects!';
+		});
+	}
 
 	// get list of projects from database
 	$scope.getAllProjects();
 }]);
 
+agileApp.controller('oneProjectController', ['$scope','agileService', function ($scope, agileService)
+{
+	$scope.getOneProject = function (p) {
+		agileService.getOneProject(p)
+
+		.then(function success(response){
+			$scope.sprints = response.data;
+			$scope.errorMessage = '';
+		},
+		function error (response ){
+			$scope.errorMessage = 'Error getting details of k4 project';
+		});
+	}
+
+	// get details of k4 project
+	$scope.getOneProject("k4");
+}]);
+
 agileApp.service('agileService',['$http', function ($http) {
-    this.getAllProjects = function getAllProjects(){
-        return $http({
-          method: 'GET',
-          url: 'http://localhost:8000/p/list'
-        });
-    }
+	this.getAllProjects = function getAllProjects(){
+		return $http({
+			method: 'GET',
+			url: 'http://localhost:8000/p/list'
+		})
+	}
+
+	this.getOneProject = function getOneProject(p) {
+		return $http({
+			method: 'GET',
+			url: 'http://localhost:8000/' + 'k4' + '/sprints/active'
+		});
+	}
 }]);
